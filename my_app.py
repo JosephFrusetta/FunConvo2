8efa47a7b877187d11dfb2d1438c1cea7a83ec91
Joseph Frusetta <JosephFrusetta@gmail.com>
2023-03-26 19:52:33 -0700

    Improved AI description
    New system description
    +Cleaned comments

--- my_app.py
+++ my_app.py
@@ -6,28 +6,23 @@ import openai
 
 app = Flask(__name__)
 
-# Set your Twilio Account SID and Auth Token as environment variables
+# Set environment variables
+openai.api_key = os.environ['OPENAI_API_KEY']
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
-
 twilio_client = Client(account_sid, auth_token)
 
-# Set your OpenAI GPT-4 API key as an environment variable
-openai.api_key = os.environ['OPENAI_API_KEY']
-
-# Create an empty list, store the chat history, and add a system message to it
-message_history = [{'role': 'system', 'content': "The following is a conversation with an AI that helps humans create interesting conversation starters. The AI is intelligent, creative, and clever. The AI will not provide boring nor conventional messages."}]
+# Create an empty list, store the chat history, and add my system message
+message_history = [{'role': 'system', 'content': "You are an AI that helps humans create interesting conversation starters. You are intelligent, creative, and clever. You will not provide conventional messages. Assume information received is about the person who we are questioning. One or two words could be our only information on them."}]
 
 @app.route('/sms', methods=['POST'])
 def sms_reply(role="user"):
-    
-    # Get the incoming SMS message / # Add the user input to the chat history
     message_history.append({"role": role, "content": request.form.get('Body', '')})   
 
     # Generate a response using GPT-3.5 API
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=message_history # Use the message history as input
+        messages=message_history
     )
 
     # Extract the generated text
