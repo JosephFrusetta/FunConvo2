4d66cd29cc0d6573822a62a358a8dde934c4b1c6
Joseph Frusetta <JosephFrusetta@gmail.com>
2023-04-12 15:45:20 -0700

    Implementing Retries
    Using tenacity to allow retries to be made in case of error.
    
    If none of the retries get through, return an error message.

--- my_app.py
+++ my_app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import openai
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 app = Flask(__name__)
 
@@ -15,15 +16,26 @@ twilio_client = Client(account_sid, auth_token)
 # Create an empty list, store the chat history, and add my system message
 message_history = [{'role': 'system', 'content': "You are an AI that is intelligent, creative, and clever. You generate two or three interesting conversation starters based on observations made and provided by the user. Observations might only be one or two words. You provide multifaceted questions(Interesting sombrero, ¿Cuál es la historia detrás de él? - What's the story behind it?), factual trivia(Did you know that many resorts outlawed snowboarding when the sport was invented back in the 80's?), and complex conundrums(If both your parents need a kidney to live and you're the only match in the world - what would you do?)."}]
 
+# Decorator from tenacity, used to configure retry logic
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=2, max=60))
+def call_openai_api(messages):
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+# Decorator from Flask, defines a route for handling incoming HTTP POST requests at the /sms endpoint.
 @app.route('/sms', methods=['POST'])
 def sms_reply(role="user"):
     message_history.append({"role": role, "content": request.form.get('Body', '').strip()})
 
     # Generate a response using GPT-3.5
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=message_history
-    )
+    try:
+        completion = call_openai_api(message_history)
+    except Exception as e:
+        error_message = f"Error: {e}. Please try again later."
+        twilio_response = MessagingResponse()
+        twilio_response.message(error_message)
+        return str(twilio_response)
 
     # Extract the generated text
     generated_text = completion.choices[0].message.content
